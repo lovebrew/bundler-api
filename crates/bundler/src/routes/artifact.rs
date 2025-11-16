304713aede7014bc3320d126280667744d9d251c
TurtleP <jpostelnek@outlook.com>
2025-11-16 14:05:57 -0500

    artifact: delete dir if empty

--- crates/bundler/src/routes/artifact.rs
+++ crates/bundler/src/routes/artifact.rs
@@ -1,9 +1,14 @@
 use std::path::{Component, PathBuf};
 
-use rocket::{fs::NamedFile, http::Status};
+use rocket::{fs::NamedFile, http::Status, tokio};
 use uuid::Uuid;
 
 use crate::routes::artifacts_dir;
+
+fn check_is_empty(path: &PathBuf) -> bool {
+    path.read_dir()
+        .map_or(false, |mut dir| dir.next().is_none())
+}
 
 #[get("/artifact?<uuid>&<filepath..>")]
 pub async fn artifact(uuid: String, filepath: String) -> Result<NamedFile, Status> {
@@ -11,16 +16,23 @@ pub async fn artifact(uuid: String, filepath: String) -> Result<NamedFile, Statu
     if Uuid::parse_str(&uuid).is_err() || filepath.is_empty() {
         return Err(Status::BadRequest);
     }
-
+    let artifact_path = base_path.join(&uuid);
     let path = PathBuf::from(filepath);
     if path.components().any(|c| matches!(c, Component::ParentDir)) {
         return Err(Status::Forbidden);
     }
 
-    let path = base_path.join(&uuid).join(path);
-    let file = NamedFile::open(path).await.map_err(|_| Status::NotFound)?;
+    let path = artifact_path.join(path);
+    let file = NamedFile::open(&path).await.map_err(|_| Status::NotFound)?;
+    if let Err(_) = tokio::fs::remove_file(&path).await {
+        return Err(Status::InternalServerError);
+    }
 
-    // TODO: check uuid directory is empty, recursively and delete it if it is
+    if check_is_empty(&artifact_path) {
+        if let Err(_) = tokio::fs::remove_dir_all(&artifact_path).await {
+            return Err(Status::InternalServerError);
+        }
+    }
 
     Ok(file)
 }
